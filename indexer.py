edfab04bb87389fc7ff85029b59f4bd895804adb
成镇宇 <cheng-zy17@mails.tsinghua.edu.cn>
2020-11-14 09:08:02 +0000

    feat: added phrase indexing

--- indexer.py
+++ indexer.py
@@ -35,18 +35,18 @@ class Indexer:
       line_count = 0
       for line in f:
         fieldtype_context = FieldType()
-        fieldtype_context.setIndexOptions(IndexOptions.DOCS_AND_FREQS)
+        fieldtype_context.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
         fieldtype_context.setStored(True)
         fieldtype_context.setTokenized(True)
 
         fieldtype_phrase = FieldType()
-        fieldtype_phrase.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
         fieldtype_phrase.setStored(True)
-        fieldtype_phrase.setTokenized(True)
+
+        processed_context, processed_phrase = self.process_line(line)
 
         doc = Document()
-        doc.add(Field('context', line, fieldtype_context))
-        doc.add(Field('phrase', line, fieldtype_phrase))
+        doc.add(Field('context', processed_context, fieldtype_context))
+        doc.add(Field('phrase', processed_phrase, fieldtype_phrase))
 
         writer.addDocument(doc)
 
@@ -57,3 +57,15 @@ class Indexer:
 
     writer.close()
     print()
+
+  def process_line(self, line):
+    processed_context = []
+    processed_phrase = []
+    terms = line.split(' ')
+    for index, term in enumerate(terms):
+      splitted = term.split('_')
+      if len(splitted) > 1:
+        processed_context.append(term)
+        processed_phrase.append(splitted[1])
+    
+    return ' '.join(processed_context), ' '.join(processed_phrase)
