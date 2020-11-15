224567ae60422dfc0fbdca8153b5128e55f60674
成镇宇 <cheng-zy17@mails.tsinghua.edu.cn>
2020-11-15 02:15:23 +0000

    feat: added restriction

--- retriever.py
+++ retriever.py
@@ -89,56 +89,34 @@ class Retriever:
       result_contexts = self.search_terms(parsed_query)
     
     self.recover_to_article(query_str, result_contexts, restriction)
-
+    
     final_result = []
     simpleHTMLFormatter = SimpleHTMLFormatter(u"<b><font color='red'>", u"</font></b>")
-    for hit in self.recovered_hits:
-      highlighter = Highlighter(simpleHTMLFormatter, QueryScorer(parsed_query))
-      highLightText = highlighter.getBestFragment(self.analyzer, 'context', hit)
+    for index, recovered_query in enumerate(self.recovered_queries):
+      highlighter = Highlighter(simpleHTMLFormatter, QueryScorer(QueryParser("context", self.analyzer).parse(recovered_query)))
+      highLightText = highlighter.getBestFragment(self.analyzer, 'context', self.recovered_contexts[index])
       if highLightText is not None:
         final_result.append(highLightText)
     
     return final_result
 
   def recover_to_article(self, query_str, contexts, restriction):
-    self.recovered_query = []
-    self.recovered_context = []
+    self.recovered_queries = []
+    self.recovered_contexts = []
 
     self.recovered_hits = []
 
     for context in contexts:
       terms = context.split(' ')
       length = len(terms)
-      self.recovered_hits.append(''.join(terms))
-      # for matched_index, term in enumerate(terms):
-      #   if term == query_str:
-      #     tmp_matched_query = self.get_restriction_query(terms, length, matched_index, restriction)
-      #     self.recovered_query.append(tmp_matched_query)
-      #     self.recovered_context.append(''.join(terms))
-
-      
-      #     tmp_matched_hit = ''
-      #     if matched_index == 0:
-      #       tmp_matched_hit += \
-      #         (terms[0]) \
-      #         + (terms[1] if terms[1] not in punctuation else '') \
-      #         + ' '
-      #     elif matched_index == 1:
-      #       tmp_matched_hit += \
-      #         (terms[0] + terms[1]) \
-      #         + (terms[2] if terms[2] not in punctuation else '') \
-      #         + ' '
-      #     elif matched_index == length-2:
-      #       tmp_matched_hit += \
-      #         (terms[length-3] + terms[length-2] \
-      #         if terms[length-3] not in punctuation else terms[length-2]) \
-      #         + ' '
-      #     elif matched_index == length-3:
-      #       tmp_matched_hit = \
-      #         (terms[length-4] + terms[length-3] + terms[length-2] \
-      #         if terms[length-4] not in punctuation else terms[length-3] + terms[length-2]) \
-      #         + ' '
-      #     elif matched_index >= 2 and matched_index <= length-4:
+      # self.recovered_hits.append(''.join(terms))
+      for matched_index, term in enumerate(terms):
+        if term in query_str:
+          joined = ''.join(terms)
+          if joined not in self.recovered_contexts:
+            tmp_matched_query = self.get_restriction_query(terms, length, matched_index, restriction)
+            self.recovered_queries.append(tmp_matched_query)
+            self.recovered_contexts.append(joined)
       
     self.recovered_hits = list(set(self.recovered_hits))
     if '' in self.recovered_hits:
